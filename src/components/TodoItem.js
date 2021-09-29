a9d682a53a64a49d2be3cef68fa123218bbf911a
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-09-29 09:28:46 -0300

    Refactor todo item to be a function component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,10 +4,8 @@
 /* eslint-disable no-undef */
 import React from 'react';
 
-class TodoItem extends React.Component {
-  render() {
-    return <li>{this.props.todo.title}</li>;
-  }
+function TodoItem(props) {
+  return <li>{props.todo.title}</li>;
 }
 
 export default TodoItem;

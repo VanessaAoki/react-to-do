57b3b9cef9ade4ebdfe3fecfcce5ce41f38413ad
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-10-23 12:50:17 -0300

    Add state to todo container

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
 class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Atividade 1",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Atividade 2",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "Atividade 3",
+        completed: true
+      },
+      {
+        id: 4,
+        title: "Atividade 4",
+        completed: true
+      },
+    ]
+  }
   render () {
     return (
-      <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </div>
+      <ul>
+        {this.state.todos.map(todo => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(<TodoContainer />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+)

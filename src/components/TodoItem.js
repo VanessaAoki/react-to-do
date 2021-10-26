f9091ee91e01ae719f2d9c0f57fa1ba7dc6e8d9f
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-10-25 22:29:29 -0300

    Add some styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import styles from './TodoItem.module.css'
 
 function TodoItem(props) {
+  const { completed, id, title } = props.todo;
+  
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
+
   return (
-    <li>
-      <input 
-        type="checkbox" 
-        checked={props.todo.completed}
-        onChange={() => props.handleChangeProps(props.todo.id)}
-      />{props.todo.title}
-      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
-        Delete
-      </button>
-    </li>
-  )
+  <li className={styles.item}>
+    <input
+      type="checkbox"
+      className={styles.checkbox}
+      checked={completed}
+      onChange={() => props.handleChangeProps(id)}
+    />
+    <button onClick={() => props.deleteTodoProps(id)}>
+      Delete
+    </button>
+    <span style={completed ? completedStyle : null}>
+      {title}
+    </span>
+  </li>
+)
 }
 
 export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 import TodoContainer from './components/TodoContainer';
 
 ReactDOM.render(

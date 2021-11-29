c09794e7d513f0f389f91fb6d37ec6ecedd8afe6
VanessaAoki <66967274+VanessaAoki@users.noreply.github.com>
2021-11-29 12:51:52 -0300

    Delete classBased folder, add linters and fix linters errors

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,65 +1,62 @@
-import React, { useState, useEffect } from "react";
-import Header from "./Header";
-import InputTodo from "./InputTodo";
-import TodosList from "./TodosList";
-import { v4 as uuidv4 } from "uuid";
+/* eslint-disable no-param-reassign */
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Header from './Header';
+import InputTodo from './InputTodo';
+import TodosList from './TodosList';
 
 const TodoContainer = () => {
   const getInitialTodos = () => {
     const temp = localStorage.getItem('todos');
     const savedTodos = JSON.parse(temp);
-    return savedTodos || []
+    return savedTodos || [];
   };
 
-  const [todos, setTodos] = useState(getInitialTodos())
+  const [todos, setTodos] = useState(getInitialTodos());
 
-  const handleChange = id => {
-    setTodos(prevState =>
-      prevState.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed,
-          }
-        }
-        return todo
-      })
-    )
+  const handleChange = (id) => {
+    setTodos((prevState) => prevState.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    }));
   };
 
-  const delTodo = id => {
+  const delTodo = (id) => {
     setTodos([
-      ...todos.filter(todo => {
-        return todo.id !== id
-      }),
-    ])
+      ...todos.filter((todo) => todo.id !== id),
+    ]);
   };
 
-  const addTodoItem = title => {
+  const addTodoItem = (title) => {
     const newTodo = {
       id: uuidv4(),
-      title: title,
+      title,
       completed: false,
-    }
-    setTodos([...todos, newTodo])
+    };
+    setTodos([...todos, newTodo]);
   };
 
   const setUpdate = (updatedTitle, id) => {
     setTodos(
-      todos.map(todo => {
+      todos.map((todo) => {
         if (todo.id === id) {
-          todo.title = updatedTitle
+          todo.title = updatedTitle;
         }
-        return todo
-      })
-    )
+        return todo;
+      }),
+    );
   };
 
   useEffect(() => {
     // storing todos items
-    const temp = JSON.stringify(todos)
-    localStorage.setItem("todos", temp)
-  }, [todos])
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+  }, [todos]);
 
   return (
     <div className="container">
@@ -74,7 +71,7 @@ const TodoContainer = () => {
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default TodoContainer;
--- src/functionBased/components/TodosList.js
+++ src/functionBased/components/TodosList.js
@@ -1,11 +1,14 @@
-import React from "react"
-import TodoItem from "./TodoItem";
+/* eslint-disable react/prop-types */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+import TodoItem from './TodoItem';
 
 class TodosList extends React.Component {
   render() {
     return (
       <ul>
-        {this.props.todos.map(todo => (
+        {this.props.todos.map((todo) => (
           <TodoItem
             key={todo.id}
             todo={todo}
@@ -15,8 +18,8 @@ class TodosList extends React.Component {
           />
         ))}
       </ul>
-    )
+    );
   }
 }
 
-export default TodosList
+export default TodosList;
